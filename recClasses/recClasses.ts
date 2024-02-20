7361740b155d4e8926c4d655e662f8080eeb87b7
Levi Ramirez <levi.ramirez135@gmail.com>
2024-02-20 11:48:01 -0800

    feat: itemCategory draft

---
+++ recClasses/recClasses.ts
@@ -0,0 +1,21 @@
+class ItemCategory {
+  itemName: string
+  categoryNames: string[]; // item's main categories
+  subCategory: string[]; // item's subcategories (MIGHT BE ABLE TO MERGE THIS WITH THE LINE ABOVE)
+  indScore: number; // individual score of the item
+
+  constructor(categoryNames: string[], subCategory: string[], indScore: number) {
+      this.categoryNames = categoryNames;
+      this.subCategory = subCategory;
+      this.indScore = indScore;
+  }
+}
+
+let SCORE_MAP: Map<string, number> = new Map()
+
+
+/*
+- Each item will be created on startup with the ItemCategory class
+- Store these items in an array of tuples [[score, ItemCategory item], ...]
+- to compute the current "score" for each item, we would need to store the current ranks of the categories and the ranking of the individual items
+*/
