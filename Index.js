cb9f284bfcaf9661016b64740589d22619fb4172
Vedaant Patel <vedaant@dhcp-v117-185.mobile.uci.edu>
2024-02-23 13:26:27 -0800

    merge

--- Index.js
+++ Index.js
@@ -13,7 +13,7 @@ import { setUser } from './redux/slices/user';
 
 import { getAuth, onAuthStateChanged, initializeAuth } from 'firebase/auth'
 
-import HelloWorldScreen from './screens/HelloWorldScreen';
+// import HelloWorldScreen from './screens/HelloWorldScreen';
 import ActivityScreen from './screens/ActivityScreen';
 
 
